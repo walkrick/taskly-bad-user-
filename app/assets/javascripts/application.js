854c28b46757975e6fced988159ced6f48783898
Rick Walker <walkrick@yahoo.com>
2014-08-12 13:03:20 -0600

    added popup box on about click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(document).ready(function () {
+  $('.about-link').on('click', function () {
+    alert('hello');
+  });
+
+});
+
+
+
