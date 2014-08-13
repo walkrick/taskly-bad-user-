5a81de4774b3be2de1540e420a55dc0247072cb9
Rick Walker <walkrick@yahoo.com>
2014-08-13 16:17:53 -0600

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,32 @@ $(document).ready(function () {
     alert('hello');
   });
 
+
+
+  var $buttons = $('[data-button]');
+  var $taskLists = $('[data-task-list]');
+  $taskLists.hide();
+
+  $buttons.on('click', function() {
+    var buttonId = this.dataset.button;
+    var $taskList = $('html').find('[data-task-list=' + buttonId + ']');
+    console.log($taskList);
+    $taskList.toggle();
+  });
+
+
+
+
+
+
+
+//var tasklists =$('.task-list button');
+//tasklists.on('click', function() {
+//  var button = $(this);
+//  var ul = button.parents('.task-list').find('ul');
+//  ul.toggle();
+//});
+
+
+
 });
-
-
-
